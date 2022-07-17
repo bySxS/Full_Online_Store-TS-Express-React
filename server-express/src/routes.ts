c9323a7730eb02ed74e5f7134e68081fa18ba6f8
bySxS <88063532+bySxS@users.noreply.github.com>
2022-07-18 01:00:57 +0300

    Update routes.ts

--- server-express/src/routes.ts
+++ server-express/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import UsersRoutes from './users/users.routes'
 import RolesRoutes from './users/roles/roles.routes'
+import ProductsRoutes from './products/products.routes'
 const router = Router()
 
 router.use('/user/roles', RolesRoutes)
 router.use('/user', UsersRoutes)
+router.use('/product', ProductsRoutes)
 
 export default router
