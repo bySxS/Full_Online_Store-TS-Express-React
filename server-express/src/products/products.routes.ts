208dba500d8d780cb597c9b47faa80e3c2bfe505
bySxS <88063532+bySxS@users.noreply.github.com>
2022-07-18 02:02:41 +0300

    Update products.routes.ts

--- server-express/src/products/products.routes.ts
+++ server-express/src/products/products.routes.ts
@@ -4,18 +4,18 @@ import { AuthMiddleware } from '../middleware/auth'
 import ApiError from '../apiError'
 import ProductsController from './products.controller'
 import { validateId } from '../users/users.validator'
-import UsersController from '../users/users.controller'
 
 const router = Router()
 
 try {
-  router.post('/add', RoleMiddleware(['admin']), ProductsController.add)
+  router.post('/add', AuthMiddleware, ProductsController.add)
   router.delete('/delete/:id', RoleMiddleware(['admin']), ProductsController.deleteById)
   router.put('/update/:id', RoleMiddleware(['admin']), ProductsController.updateById)
   router.get('/search', ProductsController.search)
+  router.get('/all', ProductsController.getAll)
   router.get('/:id', validateId(), ProductsController.getById)
 } catch (e) {
-  throw ApiError.internalRequest('Ошибка в Products routers', 'UsersRouter')
+  throw ApiError.internalRequest('Ошибка в Products routers', 'ProductsRouter')
 }
 
 export default router
