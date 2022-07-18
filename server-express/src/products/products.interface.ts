5b99affafd9eb9bcd586d9de24a5e11095ecb765
bySxS <88063532+bySxS@users.noreply.github.com>
2022-07-18 18:58:34 +0300

    Update products.interface.ts

--- server-express/src/products/products.interface.ts
+++ server-express/src/products/products.interface.ts
@@ -1,6 +1,7 @@
 import { IMessage } from '../interface'
 import { NextFunction, Request, Response } from 'express'
 import { FileArray, UploadedFile } from 'express-fileupload'
+import ProductsModel from './products.model'
 
 export interface IProduct {
   id: number,
@@ -21,6 +22,17 @@ export interface IProduct {
   image8?: string
   image9?: string
   image10?: string
+  delScreen?: boolean
+  delImage1?: boolean
+  delImage2?: boolean
+  delImage3?: boolean
+  delImage4?: boolean
+  delImage5?: boolean
+  delImage6?: boolean
+  delImage7?: boolean
+  delImage8?: boolean
+  delImage9?: boolean
+  delImage10?: boolean
   videoYoutubeUrl?: string
   parentId?: number
   url?: string
@@ -56,7 +68,9 @@ export interface IProductController {
 }
 
 export interface IProductService {
-  updatePictures: (id: number, DtoFile: IProductFilesArray) => Promise<string>
+  delPicture: (delPic: boolean, fileName: string, pathDir: string) => Promise<string>
+  savePicture: (id: number, pathDir: string, file: UploadedFile, fileName: string, fileName2: string) => Promise<string>
+  updatePictures: (id: number, DtoFile: IProductFilesArray, Dto: IProduct, findProduct: ProductsModel | null) => Promise<string>
   add: (Dto: IProduct, DtoFile: IProductFilesArray) => Promise<IMessage>
   updateById: (id: number, Dto: IProduct, DtoFile: IProductFilesArray) => Promise<IMessage>
   deleteById: (id: number) => Promise<IMessage>
