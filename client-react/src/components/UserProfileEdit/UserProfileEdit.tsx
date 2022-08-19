1e0bafbed9bafdeff75705338b27b7791aaa3a18
bySxS <88063532+bySxS@users.noreply.github.com>
2022-08-19 04:01:52 +0300

    Update UserProfileEdit.tsx

--- client-react/src/components/UserProfileEdit/UserProfileEdit.tsx
+++ client-react/src/components/UserProfileEdit/UserProfileEdit.tsx
@@ -1,23 +1,60 @@
-import React, { FC } from 'react'
-import { IUsers } from 'store/myStore/myStoreUser.interface'
+import React, { FC, useState } from 'react'
+import { Button } from 'react-bootstrap'
+import { IUser } from 'store/myStore/myStoreUser.interface'
 import Registration from '../Registration/Registration'
 import style from './UserProfileEdit.module.scss'
 
 interface UserProfileProps {
-  user: IUsers
+  user: IUser
 }
 
 const UserProfileEdit: FC<UserProfileProps> = ({ user }) => {
+  const [showEditPass, setShowEditPass] = useState<boolean>(false)
+  const [showEditAvatar, setShowEditAvatar] = useState<boolean>(false)
+
+  const clickChangeAvatar = () => {
+    setShowEditAvatar(prev => !prev)
+  }
+
+  const clickChangePass = () => {
+    setShowEditPass(prev => !prev)
+  }
+
   return (
     <div className={style.viewProfile}>
       <div className={style.sectionAvatar}>
         {user.avatar
-          ? <img className={style.avatar} src={user.avatar}/>
-          : <i className="bi bi-person-circle text-5xl text-gray-700"/>
-      }
+          ? <img className={style.avatar}
+                 alt={user.nickname}
+                 src={user.avatar}
+          />
+          : <i className="bi bi-person-circle text-9xl text-gray-700"/>
+        }
+        <div className={style.button}>
+          <Button
+            onClick={clickChangeAvatar}
+            variant="primary"
+            className={'bg-emerald-600 w-full'}>
+            {!showEditAvatar ? 'Изменить аватар' : 'Не изменять аватар'}
+          </Button>
+        </div>
+        <div className={style.button}>
+          <Button
+            onClick={clickChangePass}
+            variant="primary"
+            className={'bg-emerald-600 w-full'}>
+            {!showEditPass ? 'Изменить пароль' : 'Не изменять пароль'}
+          </Button>
+        </div>
       </div>
       <div className={style.sectionInfo}>
-        <Registration changeProfile={true} defaultInfoUser={user} />
+        <Registration
+          changeProfile={true}
+          defaultInfoUser={user}
+          showDelAvatar={showEditAvatar}
+          showEditAvatar={showEditAvatar}
+          showEditPass={showEditPass}
+        />
       </div>
     </div>
   )
