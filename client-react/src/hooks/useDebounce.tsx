d66cf0e11903f8ed2bea29cff17c991d33821033
bySxS <88063532+bySxS@users.noreply.github.com>
2022-08-21 02:07:10 +0300

    Update useDebounce.tsx

--- client-react/src/hooks/useDebounce.tsx
+++ client-react/src/hooks/useDebounce.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react'
 
-export function useDebounce (value: string, delay = 500) {
-  const [debounced, setDebounced] = useState(value)
+export function useDebounce<T> (value: T, delay = 500) {
+  const [debounced, setDebounced] = useState<T | string>(value)
 
   useEffect(() => {
     const handler =
-      setTimeout(() => setDebounced(value.trim()), delay)
+      setTimeout(() => setDebounced(
+        ((typeof value === 'string')
+          ? value.trim()
+          : value)
+      ), delay)
     return () => clearTimeout(handler)
   }, [value, delay])
 
