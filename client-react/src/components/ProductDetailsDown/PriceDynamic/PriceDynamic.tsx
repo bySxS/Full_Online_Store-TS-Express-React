c9debadc42f649c2113452edc8c702f872f3d979
bySxS <88063532+bySxS@users.noreply.github.com>
2022-08-27 05:12:23 +0300

    Update PriceDynamic.tsx

--- client-react/src/components/ProductDetailsDown/PriceDynamic/PriceDynamic.tsx
+++ client-react/src/components/ProductDetailsDown/PriceDynamic/PriceDynamic.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const
+    },
+    title: {
+      display: true,
+      text: 'Цена'
+    }
+  }
+}
+
+const labels = [
+  'January', 'February',
+  'March', 'April', 'May', 'June', 'July'
+]
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => 1000),
+      borderColor: 'rgb(0,156,255)',
+      backgroundColor: 'rgba(0,158,255,0.5)'
+    }
+  ]
+}
 
 const PriceDynamic = () => {
   return (
     <div>
-      PriceDynamic
+      <Line options={options} data={data} />
     </div>
   )
 }
