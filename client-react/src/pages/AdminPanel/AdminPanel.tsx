252605338821bab5e975f5f938aad19aff856795
bySxS <88063532+bySxS@users.noreply.github.com>
2022-08-30 02:30:37 +0300

    client started admin page

--- client-react/src/pages/AdminPanel/AdminPanel.tsx
+++ client-react/src/pages/AdminPanel/AdminPanel.tsx
@@ -2,9 +2,9 @@ import React, { FC, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { NavLink } from 'react-router-dom'
 import { RoutePath } from 'AppRouter'
-import st from 'components/SideBar/SideBar.module.scss'
 import { useBreadcrumb } from 'context/BreadcrumbContext'
 import { useAppActions } from 'hooks/useStore'
+import style from './AdminPanel.module.scss'
 
 interface AdminPanelProps {
   name: string
@@ -18,19 +18,29 @@ const AdminPanel: FC<AdminPanelProps> = ({ name }) => {
     changeFilterState({})
   }, [])
   return (
-    <div>
+    <>
       <Helmet>
         <title>{name}</title>
         <meta name="description" content="{name}" />
       </Helmet>
-      <div className="font-bold">{name}</div>
-      <NavLink to={RoutePath.USERS} className="nav-link link-dark py-1 border-bottom rounded-0">
-        <div className={`${st.link_flex} hover:text-white hover:bg-gray-400`}>
-          <div><i className="bi bi-people-fill text-2xl pr-1.5"/></div>
-          <div>Пользователи</div>
+      <div className={style.block}>
+        <div className={style.blockContent}>
+          <NavLink to={RoutePath.USERS}><i className="bi bi-people-fill text-2xl pr-1.5"/> Список пользователей</NavLink>
         </div>
-      </NavLink>
-    </div>
+        <div className={style.blockContent}>
+          <NavLink to={RoutePath.USERS}><i className="bi bi-people-fill text-2xl pr-1.5"/> Список пользователей</NavLink>
+        </div>
+        <div className={style.blockContent}>
+          <NavLink to={RoutePath.USERS}><i className="bi bi-people-fill text-2xl pr-1.5"/> Список пользователей</NavLink>
+        </div>
+        <div className={style.blockContent}>
+          <NavLink to={RoutePath.USERS}><i className="bi bi-people-fill text-2xl pr-1.5"/> Список пользователей</NavLink>
+        </div>
+        <div className={style.blockContent}>
+          <NavLink to={RoutePath.USERS}><i className="bi bi-people-fill text-2xl pr-1.5"/> Список пользователей</NavLink>
+        </div>
+      </div>
+    </>
   )
 }
 
