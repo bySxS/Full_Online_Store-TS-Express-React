84ed0a3c0ca078d9f0beea5b3077ec082d1c2c37
bySxS <88063532+bySxS@users.noreply.github.com>
2022-08-29 21:18:08 +0300

    Update MyInput.tsx

--- client-react/src/components/UI/MyInput/MyInput.tsx
+++ client-react/src/components/UI/MyInput/MyInput.tsx
@@ -1,44 +1,62 @@
-import React, { FC, ReactNode, useState } from 'react'
+import React, { Dispatch, FC, ReactNode, SetStateAction, useState } from 'react'
 import { Button, Form, InputGroup } from 'react-bootstrap'
 
 interface MyInputProps {
-  label: any
-  value: string
-  nameInput: string
+  label?: string | ReactNode
   placeholder: string
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  value: string | undefined
+  setValue?: Dispatch<SetStateAction<any>>
+  nameInput: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   icon: ReactNode
-  isValid: boolean
+  isValid?: boolean
   addButtonHide?: boolean
   disable?: boolean
   textError?: string
+  type?: 'input' | 'textarea'
   // validate?: boolean
 }
 
 const MyInput: FC<MyInputProps> = ({
   label,
-  value,
-  nameInput,
   placeholder,
+  value,
+  setValue,
+  nameInput,
   onChange,
   icon,
   addButtonHide = false,
   disable = false,
   textError,
-  isValid
+  isValid,
+  type = 'input'
   // validate = false
 }) => {
   const [showText, setShowText] = useState(true)
+
+  const handleChangeInput =
+    ({ target: { name, value }, currentTarget }: React.ChangeEvent<HTMLInputElement>) => {
+      if (setValue) {
+        setValue((prev: any) => ({ ...prev, [name]: value }
+        ))
+      }
+      currentTarget.checkValidity()
+    }
+
   return (
     <>
-      <Form.Label>{label}</Form.Label>
-      <InputGroup hasValidation={false} className={`mb-${addButtonHide ? '3' : '2'}`}>
+      {label && <Form.Label>{label}</Form.Label>}
+      <InputGroup
+        hasValidation={!!textError}
+        className={'mb-2'}
+      >
         <InputGroup.Text id="basic-addon1">
           {icon}
         </InputGroup.Text>
         <Form.Control
           required
-          onChange={onChange}
+          as={type}
+          onChange={onChange || handleChangeInput}
           className={'pr-[4.5rem] md'}
           type={showText ? 'text' : 'password'}
           value={value}
@@ -49,6 +67,7 @@ const MyInput: FC<MyInputProps> = ({
           name={nameInput}
           aria-label={nameInput}
           aria-describedby="basic-addon1"
+          style={type === 'textarea' ? { height: '130px' } : {}}
         />
         {addButtonHide &&
           <Button className={'bg-emerald-600'}
